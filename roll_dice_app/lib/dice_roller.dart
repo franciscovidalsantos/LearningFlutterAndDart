7029adfdc5fb63f44a0f12b9e80f9cb31de50f61
francisco <franciscovidalsantos@gmail.com>
2025-01-21 18:21:49 +0000

    added default dice state logic + code optimization

--- roll_dice_app/lib/dice_roller.dart
+++ roll_dice_app/lib/dice_roller.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as img; // Import the image package
 
-final randomizer = Random();
+final _random = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -16,30 +16,41 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  dynamic diceRoll = 1;
+  dynamic diceImagePath = "";
+
+  dynamic diceRoll = _random.nextInt(6) + 1;
   img.Image? _greyscaleImage; // Holds the greyscale version of the dice image
 
   @override
   void initState() {
     super.initState();
-    _loadAndConvertImage(diceRoll); // Load the initial greyscale dice
+    dictateDiceState();
+    _loadAndConvertImage(diceImagePath); // Load the initial greyscale dice
+  }
+
+  void dictateDiceState() {
+    if (diceImagePath.isEmpty) {
+      diceImagePath = 'assets/images/dice_images/dice-default-state.png';
+    } else {
+      diceImagePath = 'assets/images/dice_images/dice-$diceRoll.png';
+    }
   }
 
   void rollDice() {
     setState(() {
-      diceRoll = randomizer.nextInt(6) + 1; // random value from 1 to 6
-      print(diceRoll >= 5 ? "High Roll" : "Better Luck Next Time");
+      diceRoll = _random.nextInt(6) + 1; // random value from 1 to 6
+      // print(diceRoll >= 5 ? "High Roll" : "Better Luck Next Time");
     });
 
     // Load and convert the new dice image to greyscale
-    _loadAndConvertImage(diceRoll);
+    dictateDiceState();
+    _loadAndConvertImage(diceImagePath);
   }
 
   // Load and convert the dice image to greyscale
-  Future<void> _loadAndConvertImage(int diceRoll) async {
+  Future<void> _loadAndConvertImage(String diceImage) async {
     // Load image from assets
-    final ByteData data =
-        await rootBundle.load('assets/images/dice_images/dice-$diceRoll.png');
+    final ByteData data = await rootBundle.load(diceImage);
     final List<int> bytes = data.buffer.asUint8List();
 
     // Decode the image and convert to greyscale
@@ -60,7 +71,7 @@ class _DiceRollerState extends State<DiceRoller> {
       children: [
         SizedBox(
           width: double.infinity,
-          height: 100,
+          height: 200,
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -87,7 +98,7 @@ class _DiceRollerState extends State<DiceRoller> {
           onPressed: rollDice,
           child: Text(
             "Roll Dice",
-            style: const TextStyle(fontSize: 10, color: Colors.black),
+            style: const TextStyle(color: Colors.black),
           ),
         ),
       ],
