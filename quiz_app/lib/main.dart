0c52d3bb42b1d31865c7f7ddd681bbdc89967a77
francisco <franciscovidalsantos@gmail.com>
2025-02-03 19:22:06 +0000

    added step 1 solution

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   // Ensure Flutter bindings are initialized
@@ -10,44 +11,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/quiz-logo.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.purple[200]),
-              child: Text("daata"),
-            )
-          ],
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.yellow, Colors.green],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const StartScreen(),
         ),
-        // body: Container(
-        //   decoration: BoxDecoration(
-        //     gradient: LinearGradient(
-        //         colors: [Colors.yellow, Colors.green],
-        //         begin: Alignment.topLeft,
-        //         end: Alignment.bottomRight),
-        //   ),
-        //   child: Column(
-        //     children: [
-        //       Container(
-        //         decoration: BoxDecoration(
-        //           image: DecorationImage(
-        //             image: AssetImage('assets/images/quiz-logo.png'),
-        //             fit: BoxFit.cover,
-        //           ),
-        //         ),
-        //       ),
-
-        //       // Expanded(child: Text("Learn Flutter the fun way!"))
-        //     ],
-        //   ),
-        // ),
       ),
     ),
   );
--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -7,21 +7,47 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/quiz-logo.png'),
-              fit: BoxFit.cover,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // decoration: BoxDecoration(color: Colors.red[500]),
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+          ),
+
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 40, 20, 0),
+            // decoration: BoxDecoration(color: Colors.purple[200]),
+            child: Text(
+              "Boring text message waya :(",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
             ),
           ),
-        ),
-        Container(
-          decoration: BoxDecoration(color: Colors.purple[200]),
-          child: Text("daata"),
-        )
-      ],
+
+          Container(
+            // decoration: BoxDecoration(color: Colors.pink),
+            width: double.infinity, // stretch to screen size
+            child: SizedBox(
+              height: 40, // set space between buttons
+            ),
+          ),
+
+          Container(
+            child: OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              child: Text("Start the quiz"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
