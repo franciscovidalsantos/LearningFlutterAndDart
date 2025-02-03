00179aef5230f2cc7efa4b0b61561e94301d62e5
francisco <franciscovidalsantos@gmail.com>
2025-02-03 02:50:03 +0000

    playground - populated pages with random slider experiments

--- playground/lib/page_0.dart
+++ playground/lib/page_0.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class Page0 extends StatefulWidget {
   const Page0({super.key});
@@ -9,12 +10,66 @@ class Page0 extends StatefulWidget {
   }
 }
 
+// Currently not being used
+double degrees(double radians) => radians * (180 / math.pi);
+
 class _Page0State extends State<Page0> {
+  double scale = 1;
+  double angle = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Page 0"),
+    return Scaffold(
+      body: Container(
+        color: Colors.amber,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                child: Text("Slider that grows and rotates 90º each time"),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                child: Transform.rotate(
+                  alignment: Alignment.center,
+                  angle: angle,
+                  child: Transform.scale(
+                    alignment: Alignment.center,
+                    scale: scale,
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Slider(
+                // onChangeStart: (double startValue) {
+                //   print('Started change at $startValue');
+                // },
+                // onChangeEnd: (double endValue) {
+                //   print('Started change at $endValue');
+                // },
+                value: angle,
+                min: 0,
+                max: 360,
+                divisions: 8,
+                label: angle.toInt().toString() + "º",
+                onChanged: (newValue) => setState(() {
+                  angle = newValue;
+                  scale = 1 + (newValue / 360); // TODO: fix angle
+                }),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- playground/lib/page_1.dart
+++ playground/lib/page_1.dart
@@ -10,11 +10,43 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+  double _sliderValue = 0.0; // Initial value of the slider
+  double scale = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Page 1"),
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Slider Example'),
+      // ),
+      backgroundColor: Colors.yellow,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("data"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("data"),
+                Text("data"),
+              ],
+            ),
+            Slider(
+              value: _sliderValue,
+              min: 0,
+              max: 10,
+              divisions: 10,
+              label: _sliderValue.round().toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _sliderValue = value;
+                });
+              },
+            ),
+            Text('Slider value: $_sliderValue'),
+          ],
+        ),
       ),
     );
   }
