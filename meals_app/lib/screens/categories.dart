06e1e9b5d02c6bcdbac5222ad48e45b5bae2e8f6
francisco <franciscovidalsantos@gmail.com>
2025-02-26 18:46:52 +0000

    adding category model logic - currently displaying dummy_data

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
@@ -8,6 +10,7 @@ class Categories extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("pick your category")),
       body: GridView(
+        padding: EdgeInsets.all(20),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
@@ -15,10 +18,8 @@ class Categories extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: [
-          for (var i = 0; i <= numbers; i++)
-            Center(
-              child: Text(i.toString(), style: TextStyle(color: Colors.white)),
-            ),
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
         ],
       ),
     );
