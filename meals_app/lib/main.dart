9fd299818f737b83289adc7c738d41cc6bffbffb
francisco <franciscovidalsantos@gmail.com>
2025-03-17 19:44:34 +0000

    adding our tabs manager screen

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 // import 'package:meals_app/data/dummy_data.dart';
-import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/tabs.dart';
 // import 'package:meals_app/screens/meals.dart';
 
 final theme = ThemeData(
@@ -23,6 +23,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const Categories());
+    return MaterialApp(theme: theme, home: const TabsScreen());
   }
 }
--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -4,8 +4,8 @@ import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
-class Categories extends StatelessWidget {
-  const Categories({super.key});
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
 
   void _selectedCategory(BuildContext context, Category category) {
     final filteredMeals =
@@ -23,24 +23,21 @@ class Categories extends StatelessWidget {
   final numbers = 10;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("pick your category")),
-      body: GridView(
-        padding: EdgeInsets.all(20),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onSelectedCategory: () => _selectedCategory(context, category),
-            ),
-        ],
+    return GridView(
+      padding: EdgeInsets.all(20),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectedCategory: () => _selectedCategory(context, category),
+          ),
+      ],
     );
   }
 }
