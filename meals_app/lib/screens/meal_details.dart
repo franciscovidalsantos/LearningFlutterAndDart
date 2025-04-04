1025c6118baf5fc17cef3d267c691b1f99d9e8a4
francisco <franciscovidalsantos@gmail.com>
2025-04-04 14:23:43 +0100

    adding animations (implicit)
    - added an animatedSwitcher in order reproduce an animation while switching between 2 icons.

--- meals_app/lib/screens/meal_details.dart
+++ meals_app/lib/screens/meal_details.dart
@@ -31,7 +31,21 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: isFavorite ? Icon(Icons.star) : Icon(Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(
+                  isFavorite,
+                ), // we need to pass a key to the widget
+              ),
+            ),
           ),
         ],
       ),
