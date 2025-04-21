83f8d27852056c5fa5adb9a341a30cb91caea037
francisco <franciscovidalsantos@gmail.com>
2025-04-21 21:00:21 +0100

    location input implementation
    - fetching data from our API in LocationInput widget
    - adding a new class PlaceLocation to our Place model and propagating it to our Place class.

--- favorite_places_app/lib/widgets/location_input.dart
+++ favorite_places_app/lib/widgets/location_input.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:favorite_places_app/models/place.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,8 +13,94 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  PlaceLocation? _pickedLocation;
+  var _isGettingLocation = false;
+  final _ourApiKey = ""; // Replace with your actual API key
+
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return "";
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return "https://maps.googleapis.com/maps/api/staticmap?center$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=$_ourApiKey";
+  }
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    if (lat == null || lng == null) {
+      return;
+    }
+
+    final url = Uri.parse(
+      "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$_ourApiKey",
+    );
+    final response = await http.get(url);
+    final responseData = json.decode(response.body);
+    final address = responseData["results"][0]["formatted_address"];
+
+    setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: lat,
+        longitude: lng,
+        address: address,
+      );
+      _isGettingLocation = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      "No location chosen.",
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+        color: Theme.of(context).colorScheme.onBackground,
+      ),
+    );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
+
+    if (_isGettingLocation) {
+      previewContent = CircularProgressIndicator();
+    }
+
     return Column(
       children: [
         Container(
@@ -25,25 +116,19 @@ class _LocationInputState extends State<LocationInput> {
             ),
             borderRadius: BorderRadius.circular(8),
           ),
-          child: Text(
-            "No location chosen.",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
-          ),
+          child: previewContent,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              icon: Icon(Icons.map),
+              icon: Icon(Icons.location_on),
               label: Text("Get current Location"),
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
             ),
             TextButton.icon(
               icon: Icon(Icons.map),
-              label: Text("Get current Location"),
+              label: Text("Select on Map"),
               onPressed: () {},
             ),
           ],
