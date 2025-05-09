d1c1cf33ca2a1f53e496ebb332e1d76505fe0902
francisco <franciscovidalsantos@gmail.com>
2025-05-09 16:35:45 +0100

    loading and displaying chat messages

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,9 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("No messages found."));
+    return StreamBuilder(
+      stream:
+          FirebaseFirestore.instance
+              .collection("chat")
+              .orderBy("createdAt", descending: false)
+              .snapshots(),
+      builder: (context, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return Center(child: Text("No messages found."));
+        }
+        if (chatSnapshots.hasError) {
+          return Center(child: Text("Something went wrong..."));
+        }
+        final loadedMessages = chatSnapshots.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) {
+            return Text(loadedMessages[index].data()["text"]);
+          },
+        );
+      },
+    );
   }
 }
